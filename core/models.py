9f93c36ab91b01940ca6a71ac5f5cd9a4aca3d75
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-15 11:28:35 -0300

    feat: models init

--- core/models.py
+++ core/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import(
+    AbstractUser, 
+    BaseUserManager
+)
+
+
+class Base(models.Model):
+  """
+    Base abstract class for all models
+  """
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  class Meta:
+    abstract = True
+    
+    
+class Addres(Base):
+  """
+    Address model
+  """
+  # USER FK FIELD
+  cep = models.CharField(max_length=12)
+  city = models.CharField(max_length=50)
+  street = models.CharField(max_length=50)
+  neighborhood = models.CharField(max_length=50)
+  state = models.CharField(max_length=50)
+  
+  class Meta:
+      verbose_name = 'address'
+      verbose_name_plural = 'addresses'
+      
+  def __str__(self):
+      return f'{self.street} {self.street}'
+  
+  
+class Email(Base):
+    """
+      Email model
+    """
+    # USER FK FIELD
+    email = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name = 'email'
+        verbose_name_plural = 'emails'
+    
+    def __str__(self):
+        return f'{self.email}'
+    
