e4992c526e037dd5454dc26db3c8e83aa840f33f
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-19 08:09:45 -0300

    feat: create all views

---
+++ core/permissions.py
@@ -0,0 +1,57 @@
+from rest_framework import permissions
+
+class IsSuperUser(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        return False
+    
+
+class DeletePermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method == 'DELETE':
+            if request.user.is_superuser:
+                return True
+            return False
+        return True
+    
+
+class NormalUserGet(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        
+        if request.method == 'GET':
+            if request.user.is_authenticated:
+                return True
+            return False 
+        return False
+    
+
+class NormalUserPost(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        
+        if request.method == 'POST': 
+            if request.user.is_authenticated:
+                return True
+            return False
+        return False
+       
+
+class NormalUserGetPostPut(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        
+        if request.method in 'GET POST PUT':
+            if request.user.is_authenticated:
+                return True
+            return False
+        return False
--- core/views.py
+++ core/views.py
@@ -1,3 +1,133 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import (
+	NaturalPerson,
+	LegalPerson,
+	Address,
+ 	Email,
+	Phone,
+    Account,
+    Investment,
+    Loan,
+    Installment,
+    Card,
+    Transaction,
+)
+
+from .serializers import (
+	NaturalPersonSerializer,
+	LegalPersonSerializer,
+	AddressSerializer,
+ 	EmailSerializer,
+	PhoneSerializer,
+    AccountSerializer,
+    InvestmentSerializer,
+    LoanSerializer,
+    InstallmentSerializer,
+    CardSerializer,
+    TransactionSerializer,
+)
+
+from .permissions import (
+    IsSuperUser,
+    NormalUserGet,
+    NormalUserPost,
+    NormalUserGetPostPut
+)
+
+#NATURAL PERSON VIEW 
+class NaturalPersonViewSet(viewsets.ModelViewSet):
+    queryset = NaturalPerson.objects.all()
+    serializer_class = NaturalPersonSerializer
+    permission_classes = [
+		NormalUserGetPostPut
+	]
+    
+    
+#LEGAL PERSON VIEW
+class LegalPersonViewSet(viewsets.ModelViewSet):
+    queryset = LegalPerson.objects.all()
+    serializer_class = LegalPersonSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
+    
+#ADDRESS VIEW
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
+    
+    
+#EMAIL VIEW
+class EmailViewSet(viewsets.ModelViewSet):
+    queryset = Email.objects.all()
+    serializer_class = EmailSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
+    
+
+#PHONE VIEW
+class PhoneViewSet(viewsets.ModelViewSet):
+    queryset = Phone.objects.all()
+    serializer_class = PhoneSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
+    
+    
+#ACCOUNT VIEW
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
+    
+
+#INVESTMENT VIEW
+class InvestmentViewSet(viewsets.ModelViewSet):
+    queryset = Investment.objects.all()
+    serializer_class = InvestmentSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
+    
+    
+#LOAN VIEW
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
+    
+
+#INSTALLMENT VIEW
+class InstallmentViewSet(viewsets.ModelViewSet):
+    queryset = Installment.objects.all()
+    serializer_class = InstallmentSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
+    
+
+#CARD VIEW
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
+    
+    
+#TRANSACTION VIEW
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    permission_classes = [
+        NormalUserGetPostPut
+    ]
