cb30bd8870eac5e9ba6b22105e1a487b276fbeef
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-20 16:25:58 -0300

    feat: database create + runserver auto

--- populating.py
+++ populating.py
@@ -1,4 +1,5 @@
-import requests, os
+import requests, os, subprocess, multiprocessing
+from time import sleep
 
 BASE_URL = 'http://127.0.0.1:8056/api/v1/'
 
@@ -10,6 +11,26 @@ address_url = os.path.join(BASE_URL, 'addresses/')
 email_url = os.path.join(BASE_URL, 'emails/')
 phone_url = os.path.join(BASE_URL, 'phones/')
 account_url = os.path.join(BASE_URL, 'accounts/')
+
+
+def data_base_creation():
+	try: 
+		subprocess.run(['py', 'manage.py', 'makemigrations'], check=True)
+		subprocess.run(['py', 'manage.py', 'migrate'], check=True)
+		print('Success')
+	except subprocess.CalledProcessError as e:
+		print(e)
+	except Exception as e:
+		print(e)	
+
+
+def run_server():
+	try:
+		subprocess.run(['py', 'manage.py', 'runserver', '8056'], check=True)
+	except subprocess.CalledProcessError as e:
+		print(e)
+	except Exception as e:
+		print(e)	
 
 
 def user_create(register_number, password, picture='a'):
@@ -105,35 +126,45 @@ def create_account(headers, user, agency, number, type_a, balance, credit_limit,
 
 
 def main():
+	#CREATING DATABSE
+	data_base_creation()
+	server_process = multiprocessing.Process(target=run_server)
+	server_process.start()
+	sleep(1)
+
 	# NATURAL PERSON REGISTRASTION
-	# print(user_create(123456, "test@test"))
+	print(user_create(123456, "test@test"))
 	headers = create_headers(123456, "test@test")
-	# print(create_natural_person(headers, 123456, 'Luís', '2004-06-19', '222222', 'Beck'))
+	print(headers)
+	print(create_natural_person(headers, 123456, 'Luís', '2004-06-19', '222222', 'Beck'))
  
-	# print(user_create(1234567, "test@test"))
-	# headers = create_headers(1234567, "test@test")
-	# print(create_natural_person(headers, 1234567, 'Luís', '2004-06-19', '222222', 'Beck'))
+	print(user_create(1234567, "test@test"))
+	headers = create_headers(1234567, "test@test")
+	print(create_natural_person(headers, 1234567, 'Luís', '2004-06-19', '222222', 'Beck'))
 
 	# LEGAL PERSON REGISTRASTION
-	# print(user_create(654321, "test@test"))
-	# headers = create_headers(654321, "test@test")
-	# print(create_legal_person(headers, 654321, 'Fantasy', '2023-06-19', '1234', '4321', 'Cars'))
+	print(user_create(654321, "test@test"))
+	headers = create_headers(654321, "test@test")
+	print(create_legal_person(headers, 654321, 'Fantasy', '2023-06-19', '1234', '4321', 'Cars'))
  
-	# print(user_create(7654321, "test@test"))
-	# headers = create_headers(7654321, "test@test")
-	# print(create_legal_person(headers, 7654321, 'Fantasy', '2023-06-19', '1234', '4321', 'Cars'))
+	print(user_create(7654321, "test@test"))
+	headers = create_headers(7654321, "test@test")
+	print(create_legal_person(headers, 7654321, 'Fantasy', '2023-06-19', '1234', '4321', 'Cars'))
  
-	#ADDRESS REGISTRASTION
-	# print(create_address(headers, 123456, 'Rua Jones', '69', 'LeWhite Green', 'Varsóvia', 'Polônia', '12564789'))
+	# ADDRESS REGISTRASTION
+	print(create_address(headers, 123456, 'Rua Jones', '69', 'LeWhite Green', 'Varsóvia', 'Polônia', '12564789'))
  
-	#EMAIL REGISTRASTION
-	# print(create_email(headers, 123456, 'test@gmail.com'))
+	# EMAIL REGISTRASTION
+	print(create_email(headers, 123456, 'test@gmail.com'))
  
-	#PHONE REGISTRASTION
-	# print(create_phone(headers, 123456, '+55', '11', '1111-1111'))
+	# PHONE REGISTRASTION
+	print(create_phone(headers, 123456, '+55', '11', '1111-1111'))
  
-	#ACCOUNT REGISTRASTION
-	print(create_account(headers, 123456, 1, 1, 'Current', 10000, 1000))
+	# ACCOUNT REGISTRASTION
+	print(create_account(headers, [123456], 1, 1, 'Current', 10000.00, 1000.00))
+	print(create_account(headers, [7654321], 1, 2, 'Savings', 20175.00, 3268.56))	
+
+	# server_process.join()
 
 if __name__ == '__main__':
 	main()
