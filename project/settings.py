85ce626c7ecfae7816c5285e0c21983bd6b5ea72
LuisHBeck <beck.henrique.luis@gmail.com>
2023-11-03 13:37:23 -0300

    chore: fix secret_key

--- project/settings.py
+++ project/settings.py
@@ -27,10 +27,10 @@ dotenv.load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = bool(int(os.getenv('SECRET_KEY')))
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 0)
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
 ALLOWED_HOSTS = [
   host.strip() for host in os.getenv('ALLOWED_HOSTS', '').split(',')
