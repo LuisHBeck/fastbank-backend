fbeca5772868844e5cf77a5ee036e44bd31916e5
LuisHBeck <beck.henrique.luis@gmail.com>
2023-11-03 19:22:30 -0300

    ci: add create_superuser command

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -13,4 +13,5 @@ echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+python manage.py create_superuser
 python manage.py runserver 0.0.0.0:8000
